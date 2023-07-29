a9750e76ebcdc22e3612f1e9826257bc963cebf2
Marius Tomescu <tomescumarius@hotmail.com>
2023-07-29 22:17:33 +0200

    added nvchad

--- nvim/init.sh
+++ nvim/init.sh
@@ -1,3 +1,19 @@
-wget https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz &&
-tar -xvf nvim-linux64.tar.gz &&
-rm nvim-linux64.tar.gz
+download_neovim()
+{
+	echo "Downloading neovim"
+
+	wget https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.tar.gz &&
+	tar -xvf nvim-linux64.tar.gz &&
+	rm nvim-linux64.tar.gz
+}
+
+install_nvchad()
+{
+	echo "Installing nvchad"
+	git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+}
+
+#download_neovim
+
+install_nvchad
+
